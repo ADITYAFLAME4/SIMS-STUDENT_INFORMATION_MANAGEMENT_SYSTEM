f5209aa9e094293b0f77bbac48b49dae454667fe
ADITYAFLAME4 <akashindia572@gmail.com>
2023-06-22 15:57:33 +0530

    Recoded Script.js

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let hamMenuIcon = document.getElementById("ham-menu");
 let navBar = document.getElementById("nav-bar");
 let navLinks = navBar.querySelectorAll("li");
 
+
+
+
 hamMenuIcon.addEventListener("click", () => {
   navBar.classList.toggle("active");
   hamMenuIcon.classList.toggle("fa-times");
 });
+
+
 navLinks.forEach((navLinks) => {
   navLinks.addEventListener("click", () => {
     navBar.classList.remove("active");
